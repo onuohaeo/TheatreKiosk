1ff22b976edbe58aea279cdef0f6391c5f654da4
alexi <onuohaeo@mail.uc.edu>
2025-09-29 20:45:44 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if (age >= 21) {
+            System.out.println("You get a paper wrist band.");
+        }
+
+        input.close();
     }
 }
